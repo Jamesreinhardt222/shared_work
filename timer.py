7981bbeffbade5f0f0eac963d99341d12755e299
James Paul Reinhardt <54925215+Jamesreinhardt222@users.noreply.github.com>
2019-11-28 16:50:33 -0800

    Add files via upload
    Copied the decorator function for timing from the lecture slides.

--- timer.py
+++ timer.py
@@ -1,4 +1,11 @@
-def timer():
-    def wrapper:
+import time
 
-    re
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"finished {func.__name__!r} in {run_time:4f} secs")
+        return value
+    return wrapper
