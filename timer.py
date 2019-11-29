75bd7126e6f6de4b9ab184ff651ce0e049389b09
James Paul Reinhardt <54925215+Jamesreinhardt222@users.noreply.github.com>
2019-11-28 18:17:42 -0800

    Add files via upload
    Example of using a helper function

--- timer.py
+++ timer.py
@@ -5,16 +5,18 @@ def timer(func):
     """Print the runtime of the decorated function"""
 
     def wrapper(*args, **kwargs):
+        filename = "results.txt"
         start_time = time.perf_counter()
         value = func(*args, **kwargs)
         end_time = time.perf_counter()
         run_time = end_time - start_time
-        filename = "results.txt"
+        
         with open(filename, 'a') as file_object:
             file_object.write(f"Finished {func.__name__!r} in {run_time:.4f} secs\n")
         return value
 
     return wrapper
+
 
 @timer
 def factorial_iterative(n):
@@ -27,30 +29,25 @@ def factorial_iterative(n):
     return factorial
 
 
-def factoria1_recursive_helper(n):
-    factorial = 1
-    while True:
-        if n == 1:
-            break
-        factorial *= n
-        n -= 1
-    return factorial
-
 @timer
 def factorial_recursive(n):
     if n == 1:
         return 1
     else:
-        factorial = 1
-        factorial *= n
-        return factoria1_recursive_helper(n)
+        return factorial_recursive_helper(n - 1)
+
+
+def factorial_recursive_helper(n):
+    answer = 1
+    for number in range (1, n):
+        answer*= number
+    return answer
 
 
 def main():
-    for value in range(1, 101):
-        print(factorial_iterative(value))
-
-        print(factorial_recursive(value))
+    for n in range(1, 101):
+        print(f"factorial_iterative({n}) : {factorial_iterative(n)}\n")
+        print(f"factorial_recursive({n}) : {factorial_recursive(factorial_recursive_helper(n))}\n")
 
 
 if __name__ == "__main__":
